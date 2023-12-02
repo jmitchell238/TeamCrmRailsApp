01a232a3ddb89eed803165f8f8c887e0f5a3f7c1
James Mitchell <jmitchell238@gmail.com>
2023-12-02 15:32:47 -0600

    Add tailwind.config.js and import plugin modules
    Added a new tailwind.config.js file for managing Tailwind CSS configurations and plugin imports. This will help us make our project support the Tailwind utility classes more extensively. Additionally, the file specifies safe list classes, which tells PurgeCSS to not purge these class names. This allows for better optimization and management of CSS in the application.

---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+/** @type {import('tailwindcss').Config} */
+
+import generated from "@tailwindcss/forms";
+
+import generated0 from "@tailwindcss/aspect-ratio";
+
+import generated01 from "@tailwindcss/typography";
+
+import generated012 from "@tailwindcss/container-queries";
+
+import defaultTheme from "tailwindcss/defaultTheme";
+
+module.exports = {
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js'
+  ],
+  safelist: [
+  'bg-green-500',
+  'text-white',
+  'px-4',
+  'py-2',
+  'fixed',
+  'w-full',
+  'top-0',
+  'left-0',
+  'z-50',
+  'hidden'
+],
+    theme: {
+  extend: {
+    fontFamily: {
+      sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+    },
+  },
+},
+plugins: [
+  generated,
+  generated0,
+  generated01,
+  generated012
+],
+  variants: {
+    extend: {
+      backgroundColor: ['disabled']
+    }
+  }
+}
